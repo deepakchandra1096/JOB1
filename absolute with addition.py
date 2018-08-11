fd0b8597a30a14dad2500d7dd7c108cbfd102970
deepakchandra1096 <40872573+deepakchandra1096@users.noreply.github.com>
2018-08-11 14:46:46 +0530

    Add files via upload

---
+++ absolute with addition.py
@@ -0,0 +1,10 @@
+a=int(input("Enter your Number" ))
+if a<0:
+    print("the absolute value of ",a, "is" , -a)
+
+else:
+    print("the absolute value of ",a, "is" , a)
+
+
+sumoftwo =50+80
+print(sumoftwo)
